f3e208fc21e185d0e9f622fb7c5666142ec8ba58
JaskirtPooni <jaskirt.pooni@gmail.com>
2016-11-20 00:13:43 -0500

    added routes for getById and rating

--- product_resource.js
+++ product_resource.js
@@ -4,6 +4,17 @@ const router = new Router();
 function ProductResource(db) {
     productService = require('./product_service')(db);
 
+    function getById(req, res, next) {
+        productService.getById(req.params.id)
+        .then(results => {
+            res.send(200, results);
+        }).catch(function(err) {
+            res.send(500);
+        });
+
+        next();
+    };
+
     function get(req, res, next) {
         productService.get()
         .then(results =>{
@@ -41,9 +52,26 @@ function ProductResource(db) {
 
         next();
     };
+
+    function rate(req, res, next) {
+        var rating = parseInt(req.params.rating);
+        if(rating >= 1 && rating <= 10) {
+            productService.rate(req.params.id, rating)
+            .then(function(result) {
+                res.send(200);
+            }).catch(function(err) {
+                res.send(500);
+            });
+        }
+    
+        next();
+    };
+
+    router.get('api/products/:id', getById)
     router.get('api/products', get);
     router.post('api/products', post);
     router.del('api/products/:id', del);
+    router.put('api/products/:id/:rating', rate);
     return router;
 }
 
--- product_service.js
+++ product_service.js
@@ -4,6 +4,16 @@ function productService(db) {
         productCollection = dbClient.collection('products'),
         assert = require('assert');
 
+    function getById(productId) {
+        return productCollection.find({_id : new ObjectID(productId)}).toArray()
+        .then(results => {
+            return results;
+        })
+        .catch(err=> {
+            return err;
+        })
+    }
+
     function get() {
         return productCollection.find({}).toArray()
             .then(results => {
@@ -26,14 +36,30 @@ function productService(db) {
     function del(productId) {
         return productCollection.deleteOne({_id : new ObjectID(productId)})
         .then(results => {
-            return 'sucess';
+            return 'success';
         })
         .catch((err) => {
             return err;
         })
     }
 
-    return { get, post, del };
+    function rate(productId, rating) {
+        return getById(productId)
+        .then(product => {
+            var newNumRatings = parseInt(product[0].numRatings) + 1;
+            var newRating = (parseInt(product[0].numRatings)*parseFloat(product[0].rating) + rating) / newNumRatings;
+            
+            productCollection.updateOne({_id : new ObjectID(productId)}, 
+                                        { $set: { numRatings : newNumRatings, rating : newRating}})
+            .then(results => {
+                return 'success';
+            })
+        }).catch(function(err) {
+            res.send(500);
+        });
+    }
+
+    return {getById, get, post, del, rate };
 }
 
 module.exports = productService;
